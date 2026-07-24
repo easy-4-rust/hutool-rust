663ecd4133b8bdc47b8a1f1bb86ea6209e505917
wandl-6A72h <hiwepy@gmail.com>
2026-07-24 21:20:24 +0800

    refactor(hutool-core): split ansi_colors into 1:1 structure

--- crates/hutool-core/src/lang/ansi/ansi_colors/ansi_colors.rs
+++ crates/hutool-core/src/lang/ansi/ansi_colors/ansi_colors.rs
@@ -1,31 +1,7 @@
 //! 对齐: `cn.hutool.core.lang.ansi.AnsiColors`
 
-/// 色深
-#[derive(Debug, Clone, Copy)]
-pub enum BitDepth {
-    /// 4-bit
-    Four,
-    /// 8-bit
-    Eight,
-}
-
-/// 最近色结果
-#[derive(Debug, Clone, Copy)]
-pub struct ClosestColor {
-    /// ANSI 码
-    pub code: u8,
-}
-
-impl ClosestColor {
-    /// 转为前景/背景码
-    pub fn to_ansi_code(self, fore: bool) -> u8 {
-        if fore {
-            self.code
-        } else {
-            self.code.saturating_add(10)
-        }
-    }
-}
+use super::bit_depth::BitDepth;
+use super::closest_color::ClosestColor;
 
 /// 对齐 Java: `AnsiColors`
 pub struct AnsiColors {
